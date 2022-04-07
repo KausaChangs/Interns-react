5a626383cfac88b360034d6c7561db8746fc2e8c
Kausa Changaya <changayakausa@gmail.com>
2022-04-07 13:59:47 +0200

    second commit

---
+++ src/api/dto/intern.dto.ts
@@ -0,0 +1,12 @@
+import { InternsAPI } from "../interns.api"
+import logo from './logo.svg'
+
+export interface InternsDto{
+    id   :  number
+    email : String
+    name  : String
+    age   : number
+    salary : number
+  
+
+}
